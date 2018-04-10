b30244b1d6b66311234213732fe62688fc128992
iwalker11 <walker.i@husky.neu.edu>
2018-04-10 01:24:40 -0400

    Fixed webpage generation with formula
    It works!!!!

---
+++ src/built-in-formulas.rkt
@@ -0,0 +1,118 @@
+#lang racket
+
+(require "formula.rkt"
+         "built-in-structures.rkt")
+
+(provide
+
+ (all-from-out "built-in-structures.rkt")
+ body
+ connection
+ system
+ 
+ ;; Name -> Number
+ ;; Calculates the surface gravity on a body 
+ surface-gravity
+ 
+ ;; Name -> Number
+ ;; Calculates the escape velocity from a body 
+ escape-velocity
+
+ ;; Connection -> Number
+ ;; Calculates the period of orbit of a connection
+ kepler3-period
+ 
+ ;; Connection -> Posn
+ ;; Calculates the L1 lagrange point of a connection
+ L1
+ 
+ ;; Connection -> Posn
+ ;; Calculates the L2 lagrange point of a connection
+ L2
+
+ ;; Connection -> Posn
+ ;; Calculates the L3 lagrange point of a connection
+ L3
+
+ ;; Connection -> Posn
+ ;; Calculates the L3 lagrange point of a connection
+ L4
+
+ ;; Connection -> Posn
+ ;; Calculates the L3 lagrange point of a connection
+ L5
+)
+
+(define G 6.6740831e-11)
+
+(formula (surface-gravity body)
+         ((G G) (M_b (body-mass body)) (r (body-radius body)))
+         (/ (* G M_b) (expt r 2)))
+
+(formula (escape-velocity body)
+         ((G G) (M_b (body-mass body)) (r (body-radius body)))
+         (sqrt (/ (* 2 G M_b) r)))
+
+(formula (kepler3-period conn)
+         ((G G) (M (max (body-mass (connection-name1 conn)) (body-mass (connection-name2 conn))))
+                (r (connection-distance conn)) (\\pi pi))
+         (sqrt (/ (* 4 (expt \\pi 2) (expt r 3))
+                  (* G M))))
+
+(formula (L1 connect)
+         ((\\alpha (/ (min (body-mass (connection-name1 connect))
+                           (body-mass (connection-name2 connect)))
+                      (+ (max (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect)))
+                         (min (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect))))))
+          (R (connection-distance connect)))
+         (posn (* R (- 1 (expt (/ \\alpha 3) (/ 1 3))))
+               0))
+
+(formula (L2 connect)
+         ((\\alpha (/ (min (body-mass (connection-name1 connect))
+                           (body-mass (connection-name2 connect)))
+                      (+ (max (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect)))
+                         (min (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect))))))
+          (R (connection-distance connect)))
+         (posn (* R (+ 1 (expt (/ \\alpha 3) (/ 1 3))))
+               0))
+
+(formula (L3 connect)
+         ((\\alpha (/ (min (body-mass (connection-name1 connect))
+                           (body-mass (connection-name2 connect)))
+                      (+ (max (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect)))
+                         (min (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect))))))
+          (R (connection-distance connect)))
+         (posn (* (* -1 R) (+ 1 (* (/ 5 12) \\alpha)))
+               0))
+
+(formula (L4 connect)
+         ((M1 (max (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect))))
+          (M2 (min (body-mass (connection-name1 connect))
+                   (body-mass (connection-name2 connect))))
+          (R (connection-distance connect)))
+         (posn (* (/ R 2)
+                  (/ (- M1 M2)
+                     (+ M1 M2)))
+               (* (/ (sqrt 3) 2) R)))
+
+(formula (L5 connect)
+         ((M1 (max (body-mass (connection-name1 connect))
+                              (body-mass (connection-name2 connect))))
+          (M2 (min (body-mass (connection-name1 connect))
+                   (body-mass (connection-name2 connect))))
+          (R (connection-distance connect)))
+         (posn (* (/ R 2)
+                  (/ (- M1 M2)
+                     (+ M1 M2)))
+               (* -1 (/ (sqrt 3) 2) R)))
+
+
+#|https://web.stanford.edu/~oas/SI/SRGR/notes/SchwarzschildSolution.pdf|#
---
+++ src/built-in-structures.rkt
@@ -0,0 +1,110 @@
+#lang racket
+
+(require (for-syntax syntax/parse))
+
+(provide
+
+ ;; Name Number Number -> #<procedure:Name>
+ ;; Creates a body with a mass and radius
+ body
+
+ ;; Body -> Number
+ ;; Gets the mass of a body
+ body-mass
+
+ ;; Body -> Number
+ ;; Gets the radius of a body
+ body-radius
+
+ ;; Name Name Name Number -> #<procedure:Name>
+ ;; Creates a connection between two bodies
+ connection
+
+ ;; Connection -> Name
+ ;; Gets the name of the first body
+ connection-name1
+
+ ;; Connection -> Name
+ ;; Gets the name of the second body
+ connection-name2
+
+ ;; Connection -> Number
+ ;; Gets the distance between the two bodies of the connection
+ connection-distance
+
+ ;; Name [Listof Name] -> #<procedure:Name>
+ ;; Creates a system of connections
+ system
+
+ ;; System -> [Listof Name]
+ ;; Gets the name of the system
+ system-loname
+
+ posn
+ posn-x
+ posn-y
+ posn?)
+
+(struct posn [x y]
+    #:transparent)
+
+;; A WebPage is an outputted webpage to the user's default browser
+
+;; A Name is a name of a body, represented as an identifier
+
+(struct body-struct [mass radius])
+
+;; A Body is a (body Name Number Number)
+(define-syntax body
+  (syntax-parser
+    [(_ name:id mass radius)
+     #'(define name (body-struct mass radius))]))
+
+(define-syntax body-mass
+  (syntax-parser
+    [(_ body)
+     #'(body-struct-mass body)]))
+
+(define-syntax body-radius
+  (syntax-parser
+    [(_ body)
+     #'(body-struct-radius body)]))
+
+
+(struct connection-struct [name1 name2 distance])
+
+;; A Connection is a (struct Name Name Number)
+
+(define-syntax connection
+  (syntax-parser
+    [(_ name:id name1 name2 distance)
+     #'(define name (connection-struct name1 name2 distance))]))
+
+(define-syntax connection-name1
+  (syntax-parser
+    [(_ connection)
+     #'(connection-struct-name1 connection)]))
+
+(define-syntax connection-name2
+  (syntax-parser
+    [(_ connection)
+     #'(connection-struct-name2 connection)]))
+
+(define-syntax connection-distance
+  (syntax-parser
+    [(_ connection)
+     #'(connection-struct-distance connection)]))
+
+(struct system-struct [loname])
+
+;; A System is a (struct [Listof Name])
+
+(define-syntax system
+  (syntax-parser
+    [(_ name:id l)
+     #'(define name (system-struct l))]))
+
+(define-syntax system-loname
+  (syntax-parser
+    [(_ system)
+     #'(system-struct-loname system)]))
